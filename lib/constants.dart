16d2e5c42fc85b16c84c794404dac34e81953fc5
EmanuelEnan <emaanuelenan@gmail.com>
2022-10-29 21:46:27 +0600

    .gitignore is now working

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  // String url =
-  //     'https://api.twitter.com/2/tweets/search/recent';
-
-  String bearerToken =
-      "Bearer AAAAAAAAAAAAAAAAAAAAAD7PeAEAAAAAPKX1Rnj9LDQYDvh1ouI605qT0Lc%3DZdcBEPpDRopHycGjbve5JuYrXCx0WoS7OPh7tFyV2xM8SACVxX";
-}
